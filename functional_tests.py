fd53e5ea5d5f9afcd2757f36907d1fbe04480b2d
Folder_ <folder@FolderM.Dlink>
2020-05-17 18:14:13 +0300

    ФТ теперь проверяет возможность внести элемент списка

--- functional_tests.py
+++ functional_tests.py
@@ -1,4 +1,6 @@
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+import time
 import unittest
 
 class NewVisitorTest(unittest.TestCase):
@@ -22,22 +24,38 @@ class NewVisitorTest(unittest.TestCase):
         # Она видит, что заголовок и шапка страницы говорят о 
         # списках неотложных дел
         self.assertIn('To-Do', self.browser.title)
+        header_text = self.browser.find_element_by_tag_name('h1').text
+        self.assertIn('To-Do', header_text)
+
+        # Ей сразу же предлагается внести элемент списка
+        inputbox = self.browser.find_elements_by_id('id_new_item')
+        self.assertEqual(
+            inputbox.get_attribute('placeholder'),
+            'Enter a To-Do item'
+        )
+
+        # Она набирает в текстовом поле "Купить павлиньи перья" (ее хобби - 
+        # вязание рыболовных мушек)
+        inputbox.send_key('Купить павлиньи перья')
+
+        # Когда она нажимает enter, страница обновляется, и теперь страница
+        # содержит "1: Купить павлиньи перья" в качестве элемента списка
+        inputbox.send_key(Keys.ENTER)
+        time.sleep(1)
+
+        table = self.browser.find_elements_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertTrue(
+            any(row.text == '1: Купить павлиньи перья' for row in rows)
+        )
+
+        # Текстовое поле по-прежнему приглашает ее добавить еще один элемент.
+        # Она вводит "Сделать мушку из павлиньих перьев"
+        # (Эдит очень методична)
         self.fail('Закончить тест!')
 
 if __name__ == "__main__":
     unittest.main(warnings='ignore')
-
-# Ей сразу же предлагается внести элемент списка
-
-# Она набирает в текстовом поле "Купить павлиньи перья" (ее хобби - 
-# вязание рыболовных мушек)
-
-# Когда она нажимает enter, страница обновляется, и теперь страница
-# содержит "1: Купить павлиньи перья" в качестве элемента списка
-
-# Текстовое поле по-прежнему приглашает ее добавить еще один элемент.
-# Она вводит "Сделать мушку из павлиньих перьев"
-# (Эдит очень методична)
 
 # Страница снова обновляется, и теперь показывает оба элемента ее списка
 
