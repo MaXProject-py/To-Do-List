72e9a6da0fb292fdc17458ad9007fb5570f3e73c
Folder_ <folder@FolderM.Dlink>
2020-05-17 22:34:22 +0300

    Рефакторизировано представление домашней страницы для использования шаблона

--- lists/tests.py
+++ lists/tests.py
@@ -1,6 +1,8 @@
 from django.urls import resolve
 from django.test import TestCase
 from django.http import HttpRequest
+from django.template.loader import render_to_string
+import string
 
 from lists.views import home_page
 
@@ -8,16 +10,9 @@ from lists.views import home_page
 class HomePageTest(TestCase):
     '''Тест домашней страницы'''
 
-    def test_root_url_resolves_to_home_page_views(self):
-        '''тест: корневой url преобразуется в представление домашней страницы'''
-        found = resolve('/')
-        self.assertEqual(found.func, home_page)
-
     def test_home_page_returns_correct_html(self):
         '''тест: домашняя страница возвращает правильный html'''
-        request = HttpRequest()
-        response = home_page(request)
-        html = response.content.decode('utf-8')
-        self.assertTrue(html.startswith('<html>'))
-        self.assertIn('<title>To-Do lists</title>', html)
-        self.assertTrue(html.endswith('</html>'))
+        response = self.client.get('/') 
+        # expected_html = render_to_string('home.html')
+        # self.assertEqual(html, expected_html)
+        self.assertTemplateUsed(response, 'home.html')
