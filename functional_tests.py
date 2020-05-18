6f06c16debde5f1108b99832ec004c5c127b6be1
Folder_ <folder@FolderM.Dlink>
2020-05-18 19:55:04 +0300

    Реализована функция обработки POST-запроса на сервере

--- functional_tests.py
+++ functional_tests.py
@@ -45,11 +45,10 @@ class NewVisitorTest(unittest.TestCase):
 
         table = self.browser.find_element_by_id('id_list_table')
         rows = table.find_elements_by_tag_name('tr')
-        self.assertTrue(
-            any(row.text == '1: Купить павлиньи перья' for row in rows),
-            "Новый элемент списка не появился в таблице"
 
-        )
+        self.assertIn('1: Купить павлиньи перья', [row.text for row in rows])
+        self.assertIn('2: Сделать мушку из павлиньих перьев',[row.text for row in rows])
+
 
         # Текстовое поле по-прежнему приглашает ее добавить еще один элемент.
         # Она вводит "Сделать мушку из павлиньих перьев"
